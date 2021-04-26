59cf837e694dfd6ec481a986d2a9d65861baaab6
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-26 19:19:29 +0300

    Add models for posts, tags and categories.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    """Class which save information about categories for posts."""
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Tag(models.Model):
+    """Class which save information about tags which connect to posts."""
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, verbose_name='url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Post(models.Model):
+    """Class which save information about posts."""
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='url', unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,
+                                 related_name='posts')  # Connect model Category to it.
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')  # Connect model Tag to it.
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
