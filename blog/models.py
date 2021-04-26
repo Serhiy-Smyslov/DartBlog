6ddb671daed986d7bfdb558cf7660f070422b758
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-26 22:30:28 +0300

    Add models in admin panel and add ckeditor, debuger in project.

--- blog/models.py
+++ blog/models.py
@@ -38,6 +38,7 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
     category = models.ForeignKey(Category, on_delete=models.PROTECT,
                                  related_name='posts')  # Connect model Category to it.
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')  # Connect model Tag to it.
