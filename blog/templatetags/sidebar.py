858405905bd7314f9d2fec45ab80c930b3de081a
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-27 18:13:54 +0300

    Add tag sidebar on post page.

--- blog/templatetags/sidebar.py
+++ blog/templatetags/sidebar.py
@@ -6,6 +6,13 @@ register = template.Library()
 
 @register.inclusion_tag('blog/popular_posts_tpl.html')
 def get_popular(cnt='3'):
+    """Return popular posts on platform."""
     posts = Post.objects.order_by('-views')[:cnt]
     return {'posts': posts}
 
+
+@register.inclusion_tag('blog/tags_tpl.html')
+def get_tags():
+    """Return all tags on platform."""
+    tags = Tag.objects.all()
+    return {'tags': tags}
