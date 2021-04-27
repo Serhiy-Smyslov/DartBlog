89da2b4f7c1b310eb5638ad8d5bc56d0d38ac4d8
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-27 18:51:28 +0300

    Add search for posts on post page.

--- blog/views.py
+++ blog/views.py
@@ -56,6 +56,7 @@ class PostsByTag(ListView):
 
 
 class GetPost(DetailView):
+    """Show detail information about post."""
     model = Post
     template_name = 'blog/single.html'
     context_object_name = 'post'
@@ -65,4 +66,18 @@ class GetPost(DetailView):
         self.object.views = F('views') + 1
         self.object.save()
         self.object.refresh_from_db()
+        return context
+
+
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['s'] = f's={self.request.GET.get("s")}&'
         return context
