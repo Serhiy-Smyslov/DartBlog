e46b4e03e5f7c1b307f6184ae8433cc0f34597f4
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-27 10:02:32 +0300

    Update view admin panel.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django import forms
+from django.utils.safestring import mark_safe
 
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
@@ -7,6 +8,7 @@ from .models import *
 
 
 class PostAdminForm(forms.ModelForm):
+    """Connect Ckeditor in form post of admin panel."""
     content = forms.CharField(widget=CKEditorUploadingWidget())
 
     class Meta:
@@ -15,8 +17,24 @@ class PostAdminForm(forms.ModelForm):
 
 
 class PostAdmin(admin.ModelAdmin):
+    """Setting view of post in admin panel."""
     form = PostAdminForm
     prepopulated_fields = {'slug': ('title',)}
+    save_as = True
+    save_on_top = True
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
+    list_display_links = ('id', 'title', 'slug')
+    search_fields = ('title',)
+    list_filter = ('category',)
+    readonly_fields = ('views', 'created_at', 'get_photo')
+    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '-'
+
+    get_photo.short_description = 'Фото'
 
 
 class TagAdmin(admin.ModelAdmin):
